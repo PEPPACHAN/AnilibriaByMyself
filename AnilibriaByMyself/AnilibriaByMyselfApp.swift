843c0ffdbc145e16132650e9230e9d4f14b8dc82
PEPPACHAN <PEPPACHAN@users.noreply.github.com>
2024-07-03 10:16:34 +0300

    Initial Commit

---
+++ AnilibriaByMyself/AnilibriaByMyselfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnilibriaByMyselfApp.swift
+//  AnilibriaByMyself
+//
+//  Created by PEPPA CHAN on 03.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnilibriaByMyselfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
